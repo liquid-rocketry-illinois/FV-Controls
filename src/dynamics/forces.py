273608778b660594d174ee903f835aa71004a233
Jed Ng <jedng555@gmail.com>
2026-01-22 20:33:42 -0600

    started forces and moments

---
+++ src/dynamics/forces.py
@@ -0,0 +1,11 @@
+import sympy as sp
+from sympy import *
+import numpy as np
+import pandas as pd
+from typing import Callable
+from enum import Enum
+import os
+
+class Forces():
+    def __init__(self):
+        self.forcename = ""

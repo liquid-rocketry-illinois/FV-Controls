527299d4410c8c2cbf3b7248a1ca878738e3af3a
Jed Ng <jedng555@gmail.com>
2026-01-22 20:58:44 -0600

    made some changes

--- src/dynamics/forces.py
+++ src/dynamics/forces.py
@@ -11,3 +11,25 @@ class Forces():
         self.forcename = ""
 
         self.g = 9.81
+        self.f : Matrix = None
+
+
+
+        self.f = sum() # Sum of forces 
+
+    def get_thrust(self, t: float) -> Matrix:
+        """Get the thrust for the rocket at time t.
+
+        Args:
+            t (float): The time in seconds.
+
+        Returns:
+            dict: A dictionary containing inertia, mass, CG, and thrust at time t.
+        """
+
+        T = Matrix([0., 0., 0.])  # N
+        motor_burnout = t > self.t_motor_burnout
+        if not motor_burnout:
+            T[2] = np.interp(t, self.thrust_times, self.thrust_forces) # Thrust acting in z direction
+            
+        return T
